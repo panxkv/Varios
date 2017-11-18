d56969597a6f38573ffb1998817b61feb9a788d3
Kamil Kiedrzyn <kamil.kiedrzyn@gmail.com>
2017-11-18 14:01:30 +0100

    Add exeption handling.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
 end
+
+OmniAuth.config.on_failure = Proc.new do |env|
+  ConnectionsController.action(:omniauth_failure).call(env)
+end
