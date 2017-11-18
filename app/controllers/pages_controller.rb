2eb1169ce623f9a1a5bac8bd3c1009477a213e88
Kamil Kiedrzyn <kamil.kiedrzyn@gmail.com>
2017-11-18 13:24:03 +0100

    Add dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,13 @@
 class PagesController < ApplicationController
-  def home; end
+  before_action :authenticate_user!, only: [:dashboard]
+
+  def home
+    if current_user
+      redirect_to dashboard_path
+    end
+  end
+
+  def dashboard
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'auth/:provider/callback', to: 'connections#create'
+  resources :connections, only: [:destroy]
+
   devise_for :users, controllers: { registrations: 'registrations'}
   root 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
